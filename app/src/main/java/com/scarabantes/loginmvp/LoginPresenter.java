27b79fa92cdc2760d88743026c9d759f12239063
Sergio Carabantes <sergio.carabantes@gmail.com>
2015-10-12 17:08:03 +0200

    Create login xml

---
+++ app/src/main/java/com/scarabantes/loginmvp/LoginPresenter.java
@@ -0,0 +1,9 @@
+package com.scarabantes.loginmvp;
+
+/**
+ * Created by scarabantes on 12/10/15.
+ */
+public interface LoginPresenter {
+    void create();
+    void setView(LoginView view);
+}
---
+++ app/src/main/java/com/scarabantes/loginmvp/LoginView.java
@@ -0,0 +1,8 @@
+package com.scarabantes.loginmvp;
+
+/**
+ * Created by sergio on 8/10/15.
+ */
+public interface LoginView {
+     void checkUserName(boolean exist);
+}
